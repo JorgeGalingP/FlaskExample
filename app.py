b735659074fa4e24f0f0924904197a1273c87801
JorgeGalingP <jorgape225@gmail.com>
2020-03-17 00:43:58 +0100

    Added methods to test heroku deployment

--- app.py
+++ app.py
@@ -7,9 +7,15 @@ def hello():
     return "Hello World!"
 
 
-@app.route('/<name>')
+@app.route('/name/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+
+@app.route('/number/<number>')
+def hello_sum(number):
+    return "Hello {}!".format(int(number) + int(number))
+
 
 if __name__ == '__main__':
     app.run()
