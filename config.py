978a5de3b4e320a26c00774024a3ee0c3231c70b
JorgeGalingP <jorgape225@gmail.com>
2020-03-21 14:59:37 +0100

    Added config and APP_SETTINGS constant

--- config.py
+++ config.py
@@ -13,11 +13,6 @@ class ProductionConfig(Config):
     DEBUG = False
 
 
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
