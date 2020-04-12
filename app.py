c20b411bdb16b13d35c9998bb8097dcc038897d5
JorgeGalingP <jorgape225@gmail.com>
2020-04-12 02:00:01 +0200

    Basic wordcount funcionality with redis job system

--- app.py
+++ app.py
@@ -3,57 +3,110 @@ import requests
 import operator
 import re
 import nltk
-import service
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
+from rq import Queue
+from rq.job import Job
+from worker import conn
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+CORS(app)
 db = SQLAlchemy(app)
+q = Queue(connection=conn)
 
 from models import Result
 
 
+def count_and_save_words(url):
+
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    # text processing
+    raw = BeautifulSoup(r.text).get_text()
+    nltk.data.path.append('./nltk_data/')  # set the path
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+
+    # remove punctuation, count raw words
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save the results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count
+        )
+
+        db.session.add(result)
+        db.session.commit()
+
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
     results = {}
 
     if request.method == "POST":
+        # this import solves a rq bug which currently exists
+        from app import count_and_save_words
+
         # get url that the person has entered
-        try:
-            url = request.form['url']
-            length = int(request.form['length'])
-            r = requests.get(url)
-        except:
-            errors.append("Unable to get URL. Please make sure it's valid and try again.")
-            return render_template('index.html', errors=errors)
-        if r:
-            # get count data from service
-            data = service.count_words(r, length)
-            
-            # get results
-            results = data['beauty_result']
+        url = request.form['url']
 
-            try:
-                # save result in db
-                result = Result(
-                    url=url,
-                    result_all=data['raw_word_count'],
-                    result_no_stop_words=data['no_stop_words_count']
-                )
+        if not url[:8].startswith(('https://', 'http://')):
+            url = 'http://' + url
+        job = q.enqueue_call(
+            func=count_and_save_words, # func to add a new job to the queue
+            args=(url,), # arguments to that func
+            result_ttl=5000 # hold on to the result of the job for 5000 seconds
+        )
 
-                db.session.add(result)
-                db.session.commit()
-            except:
-                errors.append("Unable to add item to database.")
+        print(job.get_id())
 
-    return render_template('index.html', errors=errors, results=results)
+    return render_template('index.html', results=results)
+
+
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )[:10]
+
+        return jsonify(results) 
+    else:
+        return "Nay!", 202
 
 
 if __name__ == '__main__':
